8f608167440b3002867c0210729b57e3fefa2b4d
Andrew Chen <axche@amazon.com>
2019-05-07 16:45:33 -0700

    work on display

--- calculator/src/component/Display.js
+++ calculator/src/component/Display.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 class Display extends React.Component {
     render() {
-    return (
-        <div>
-            <p>Display</p>
-        </div>
-    );
-    };
+        return (
+            <div>
+                <p>{this.props.value}</p>
+            </div>
+        );
+    }
 }
 
+Display.propTypes = {
+    value: PropTypes.string,
+}
 export default Display;
