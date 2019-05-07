99e8d5514e9613510fa44f37ef65c895a4cd54c7
Andrew Chen <axche@amazon.com>
2019-05-07 16:42:56 -0700

    add buttons

--- calculator/src/component/ButtonPanel.js
+++ calculator/src/component/ButtonPanel.js
@@ -1,9 +1,38 @@
+import Button from './Button';
 import React from 'react';
 class ButtonPanel extends React.Component {
     render() {
         return (
-            <div>
-                <p>Button Panel</p>
+            <div className='component-button-panel'>
+                <div>
+                    <Button name="AC"/>
+                    <Button name="+/-"/>
+                    <Button name="%"/>
+                    <Button name="/"/>
+                </div>
+                <div>
+                    <Button name="7"/>
+                    <Button name="8"/>
+                    <Button name="9"/>
+                    <Button name="*"/>
+                </div>
+                <div>
+                    <Button name="4"/>
+                    <Button name="5"/>
+                    <Button name="6"/>
+                    <Button name="-"/>
+                </div>
+                <div>
+                    <Button name="1"/>
+                    <Button name="2"/>
+                    <Button name="3"/>
+                    <Button name="+"/>
+                </div>
+                <div>
+                    <Button name="0"/>
+                    <Button name="."/>
+                    <Button name="="/>
+                </div>
             </div>
         );
     }
